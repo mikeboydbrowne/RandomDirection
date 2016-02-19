8ecf50694247cb74310281dc7ad375b70c6b9c6e
Mike Browne <michaelboydbrowne@gmail.com>
2016-02-19 15:54:06 -0500

    Initial Commit

---
+++ RandomDirection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RandomDirection
+//
+//  Created by Michael Browne on 2/19/16.
+//  Copyright © 2016 Michael Browne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RandomDirection/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RandomDirection
+//
+//  Created by Michael Browne on 2/19/16.
+//  Copyright © 2016 Michael Browne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
